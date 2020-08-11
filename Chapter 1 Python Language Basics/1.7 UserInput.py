277f4dc5b92d1e8f4183356b20d395ae87b693d8
Harry Rogers <35311763+Harry-Rogers@users.noreply.github.com>
2020-08-11 12:00:14 +0100

    Add files via upload

---
+++ Chapter 1 Python Language Basics/1.7 UserInput.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 26 14:09:12 2020
+
+@author: Harry
+"""
+
+name = input("Name please: ")
+print(name)
+
+val = input("Enter Num: ")
+# input is a str so have to convert after before printing could use float or double depeneding on input
+print(int(val))
+
+# use try/except for error handling 
