46ad2fdb0d17251ba09d90db9c8d5119b8dfc37d
Harry Rogers <35311763+Harry-Rogers@users.noreply.github.com>
2020-08-12 13:37:42 +0100

    Add files via upload

---
+++ Chapter 2 Data Types/2.5 Dictionary.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 26 15:24:43 2020
+
+@author: Harry
+"""
+
+dic = {"name":"red", "age":10}
+print(dic)
+print(dic["name"])
+print(dic.values())
+print(dic.keys())
+#keys are the names of stuff whereas values are assinged to keys 
+#can print specific keys in dictionary 
