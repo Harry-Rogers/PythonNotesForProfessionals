15b4773f3542fb543407382af2ec5a6ef201d3ff
Harry Rogers <35311763+Harry-Rogers@users.noreply.github.com>
2020-08-12 13:37:07 +0100

    Add files via upload

---
+++ Chapter 2 Data Types/2.3 Numbers.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 26 15:16:41 2020
+
+@author: Harry
+"""
+
+int_num = 10
+float_num = 10.2
+complex_num = 3.14j
+long_num = 1234567
+print(int)
+print(float)
+print(complex)
