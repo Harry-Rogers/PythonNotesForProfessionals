806095a4e39f320558832ecc6d4f080208986a59
Harry Rogers <35311763+Harry-Rogers@users.noreply.github.com>
2020-08-12 13:37:56 +0100

    Add files via upload

---
+++ Chapter 2 Data Types/2.6 Tuples.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 26 15:34:12 2020
+
+@author: Harry
+"""
+
+tuple = (123, 'hello')
+tuple1 = ('world')
+print(tuple)
+print(tuple[0])
+# tuples cant be concatenated togther or updated/ addded too similar to frozenset
