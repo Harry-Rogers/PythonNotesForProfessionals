1b769808566623bedc12e0545eb84700c7e7f0b9
Harry Rogers <35311763+Harry-Rogers@users.noreply.github.com>
2020-08-12 13:37:26 +0100

    Add files via upload

---
+++ Chapter 2 Data Types/2.4 List.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 26 15:19:31 2020
+
+@author: Harry
+"""
+
+list = [123, 'abcd', 10.2, 'd']
+list1 = ["hello", "world"]
+print(list)
+print(list[0:2])
+print(list1 *2)
+print(list + list1)
+#similar behaviours as strings but lists can involve multiple types
