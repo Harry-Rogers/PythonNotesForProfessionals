ff2c8a0b190438df973b27e1289794ba4332858d
Harry Rogers <35311763+Harry-Rogers@users.noreply.github.com>
2020-08-13 15:58:43 +0100

    Add files via upload

---
+++ Chapter 5 Date and time/5.4 Basic datetime objects usage.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 26 17:00:13 2020
+
+@author: Harry
+"""
+
+import datetime
+today = datetime.date.today()
+noon = datetime.time(12,0,0)
+now = datetime.datetime.now()
+print(today)
+print(noon)
+print(now)
+#date, time and datetime are different cant do operations unless the same
+#can convert betweenuse documentation as unlikely to be used
