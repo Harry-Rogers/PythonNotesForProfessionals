3feead10c004fd9763d604d53793d41eb3dfdd34
Harry Rogers <35311763+Harry-Rogers@users.noreply.github.com>
2020-08-13 15:57:20 +0100

    Add files via upload

---
+++ Chapter 5 Date and time/5.1 Parsing a string into timezone aware datetime object.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 26 15:41:02 2020
+
+@author: Harry
+"""
+
+import datetime
+dt = datetime.datetime.now()
+print(dt)
+#prints datetime now as year month date hour min second etc
