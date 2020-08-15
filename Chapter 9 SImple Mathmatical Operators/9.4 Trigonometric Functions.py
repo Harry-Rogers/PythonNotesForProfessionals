c86a06ae4fff3ae190d2124f40f1abe487dd9898
Harry Rogers <35311763+Harry-Rogers@users.noreply.github.com>
2020-08-15 14:35:21 +0100

    Add files via upload

---
+++ Chapter 9 SImple Mathmatical Operators/9.4 Trigonometric Functions.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 27 16:43:59 2020
+
+@author: Harry
+"""
+
+a,b = 1,2
+import math
+print(math.sin(a))
+#prints sin(1) in rads
+print(math.cosh(b))
+#prints cosh(2) in rads
+print(math.atan(math.pi))
+#prints arc tan of pi in rads
+print(math.hypot(a,b))
+#prints euclidean norm same as math.sqrt(a*a + b*b)
+print(math.degrees(a))
+#prints degrees instead of rads
