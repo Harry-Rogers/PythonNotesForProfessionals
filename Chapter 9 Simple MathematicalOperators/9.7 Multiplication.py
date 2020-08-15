12e53cce06fae695036d0fba7ffdef9f49c8ba5e
Harry Rogers <35311763+Harry-Rogers@users.noreply.github.com>
2020-08-15 14:38:36 +0100

    Add files via upload

---
+++ Chapter 9 Simple MathematicalOperators/9.7 Multiplication.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 27 16:59:13 2020
+
+@author: Harry
+"""
+
+a,b =2,3
+print(a*b)
+import operator
+print(operator.mul(a,b))
+
+print(3* "ab")
+#can multiply strings lists and tuples
+#can also be char
---
+++ Chapter 9 Simple MathematicalOperators/9.9 Modulus.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 27 17:12:44 2020
+
+@author: Harry
+"""
+
+a = 3%4
+print(a)
+b = 10%2
+print(b)
+import operator
+g = operator.mod(3,4)
+print(g)
+quotient, remainder = divmod(9,4)
+print(quotient, remainder)
+#modulus and stuff
