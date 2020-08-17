97a03b8486e0f996bf68c7b44f6c47baa91d0f98
Harry Rogers <35311763+Harry-Rogers@users.noreply.github.com>
2020-08-17 17:09:25 +0100

    Add files via upload

---
+++ Chapter 12 Operator precedence/12.1 Operator precedence BIDMAS.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 27 18:21:47 2020
+
+@author: Harry
+"""
+
+a,b,c,d = 2,3,5,7
+print(a**(b+c))
+#follow the rules of bidmas
+#brackets
+#indicies
+#multiplication
+#addition
+#subtraction
+q = a + b * c/d
+print(q)
