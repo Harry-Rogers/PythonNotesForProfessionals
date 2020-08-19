adce9f77896511be8a1cdb09b1196166b891a16d
Harry Rogers <35311763+Harry-Rogers@users.noreply.github.com>
2020-08-19 18:12:15 +0100

    Create 18.1 Lists in lists.py

---
+++ Chapter 18 Multidimensional Arrays/18.1 Lists in lists.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 29 13:55:32 2020
+
+@author: Harry
+"""
+
+lst = [[1,2,3],[4,5,6],[7,8,9]]
+print(lst[0]) # gen list
+print(lst[0][0])# specific item
+lst2 = [[[1],[2],[3]],[4],[5],[6]]
+print(lst2[0][0][0])
+#multi list ie 3-dimensional array
