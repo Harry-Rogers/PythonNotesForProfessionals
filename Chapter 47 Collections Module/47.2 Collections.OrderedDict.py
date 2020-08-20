ee7d41a6c665340d14d4f40c5f38307e3a1941c8
Harry Rogers <35311763+Harry-Rogers@users.noreply.github.com>
2020-08-20 15:21:24 +0100

    Add files via upload

---
+++ Chapter 47 Collections Module/47.2 Collections.OrderedDict.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  2 11:40:43 2020
+
+@author: Harry
+"""
+
+from collections import OrderedDict
+d = {'foo':5,'bar':6}
+print(d)
+OrderedDict([('foo',5),('bar',6)])
