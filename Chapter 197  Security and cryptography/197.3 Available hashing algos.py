61bd5a89deafa118eeff4ebaf6bc03f90f4de514
Harry Rogers <35311763+Harry-Rogers@users.noreply.github.com>
2020-09-03 14:21:32 +0100

    Add files via upload

---
+++ Chapter 197  Security and cryptography/197.3 Available hashing algos.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 18 12:51:11 2020
+
+@author: Harry
+"""
+
+import hashlib
+print(hashlib.algorithms_available)
+print(hashlib.algorithms_guaranteed)
